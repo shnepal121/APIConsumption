4ada1bb4e4d7141b4de2d790ca353ad8c4fc53c2
shnepal121 <56094894+shnepal121@users.noreply.github.com>
2020-07-01 14:17:36 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using APIConsumption.Models;
+//using APIConsumption.APIHandlerManager;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+
+
+namespace APIConsumption.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // Here Httpclient is Property of controller, not action method.
+        // since it can be used by multiple action methods. 
+        HttpClient httpClient;
+        static string URL = "https://developer.nps.gov/api/v1/";
+        static string API_KEY = "FepPmvoKkbosZSNCgSAUJmt8kltghv7apD6Ua5gk";
+
+        public IActionResult Index()
+        {
+            // Defining httpclient object & Initializing. 
+            httpClient = new HttpClient();
+
+            // Additonal parameter client sends will be cleared
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+
+            // Defining API Key Pair. 
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", API_KEY);
+
+
+            // To tell Server we can handle json. 
+            httpClient.DefaultRequestHeaders.Accept.Add(
+            new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            // Creating URL of the Park where Object need to connect. 
+            // Creating Base Address. 
+
+            string National_Park_API_Path = URL + "/parks?limit=20";
+            string parksdata = "";
+            Park parks = null; // Creating object Parks (From Model).  
+
+           
+
+            httpClient.BaseAddress = new Uri(National_Park_API_Path);
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(National_Park_API_Path).GetAwaiter().GetResult();
+                if (response.IsSuccessStatusCode)
+                {
+                    // Asynchronous for wait till get result, common for remote operation                    
+                    parksdata = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                }
+
+                if (!parksdata.Equals(""))
+                {
+                    // Deserialize parksdata string into the Parks Object. 
+                    parks = JsonConvert.DeserializeObject<Park>(parksdata);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return View(parks);
+
+
+        }
+
+        public IActionResult Parks()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Parks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIConsumption.Models
+{
+    public class Park
+    {
+        public string total { get; set; }
+        public Parks[] data { get; set; }
+        public string limit { get; set; }
+        public string start { get; set; }
+    }
+
+    public class Parks
+    {
+        public string states { get; set; }
+        public string longitude { get; set; }       
+        public string directionsInfo { get; set; }        
+        public string directionsUrl { get; set; }
+        public string url { get; set; }
+        public string weatherInfo { get; set; }
+        public string name { get; set; }        
+        public string latLong { get; set; }
+        public string description { get; set; }       
+        public string designation { get; set; }
+        public string parkCode { get; set; }        
+        public string id { get; set; }
+        public string fullName { get; set; }
+        public string latitude { get; set; }
+    }
+
+    
+
+}
